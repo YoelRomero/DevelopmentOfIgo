dfe4f739dccf292ee132b61b1a3e429851207bb0
YoelRomero <dost22@yandex.ru>
2024-05-02 12:55:15 +0000

    Fixed config-file, which will use in the next

--- main.tf
+++ main.tf
@@ -10,65 +10,91 @@ terraform {
 provider "yandex" {
   zone = "ru-central1-d"
 }
+########################################################
 
-resource "yandex_compute_disk" "boot-disk" {
-  name = "example-disk"
-  size = 5
+#Images
+resource "yandex_compute_image" "ubuntu_2004" {
+  source_family = "ubuntu-2004-lts"
 }
-
-resource "yandex_compute_instance" "example" {
-  name        = "example"
+#Disk
+resource "yandex_compute_disk" "boot-disk-vm1" {
+  name		= "boot-disk-1"
+  type		= "network-hdd"
+  zone		= "ru-central1-d"
+  size		= "10"
+  image_id	= yandex_compute_image.ubuntu_2004.id
+}
+#VM parameters
+resource "yandex_compute_instance" "vm-1" {
+  name = "terraform1"
   platform_id = "standard-v2"
-  zone        = "ru-central1-d"
-
   resources {
-    cores  = 2
+    cores = 2
     memory = 2
   }
 
   boot_disk {
-    disk_id = yandex_compute_disk.boot-disk.id
+    disk_id = yandex_compute_disk.boot-disk-vm1.id
   }
 
   network_interface {
-    subnet_id = "${yandex_vpc_subnet.foo.id}"
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
   }
 
   metadata = {
-    foo = "bar"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "echo 'Hello, World!' > index.html",
-      "nohup busybox httpd -f -p 8080 &"
-    ]
-  }
+    user-data = <<-EOF
+      #!/bin/bash
+      echo "Hello, IGO, you are nuclear-engineer! ░░░░░░░░░░░▄▄▄▄▄▄▄▄▄▄▄▄░░░░░░░░░░░
+                                                  ░░░░░░░▄▄██▀▀▀▀▀▀▀▀▀▀▀▀██▄▄░░░░░░░
+                                                  ░░░░░▄██▀▄░░░░░░░░░░░░░░▄▀██▄░░░░░
+                                                  ░░░▄██▀▄███░░░░░░░░░░░░███▄▀██▄░░░
+                                                  ░░██▀▄██████░░░░░░░░░░██████▄▀██░░
+                                                  ░██░█████████▄░░░░░░▄█████████░██░
+                                                  ██░███████████▄░░░░▄███████████░██
+                                                  ██▄███████████▀▄▄▄▄▀███████████▄██
+                                                  █████████████░██████░█████████████
+                                                  ██░░░░░░░░░░░░██████░░░░░░░░░░░░██
+                                                  ██░░░░░░░░░░░░░▀▀▀▀░░░░░░░░░░░░░██
+                                                  ▀█░░░░░░░░░░░░██████▄░░░░░░░░░░░██
+                                                  ░██░░░░░░░░░▄████████▄░░░░░░░░░██░
+                                                  ░░██▄░░░░░░▄██████████▄░░░░░░▄██░░
+                                                  ░░░▀██▄░░░▄████████████▄░░░▄██▀░░░
+                                                  ░░░░░▀██▄░▀████████████▀░▄██▀░░░░░
+                                                  ░░░░░░░▀▀██▄▄▄██████▄▄▄██▀▀░░░░░░░ 
+                                                  ░░░░░░░░░░░▀▀▀▀▀▀▀▀▀▀▀▀░░░░░░░░░░░" > index.html
+      nohup busybox httpd -f -p 8080 &
+      EOF
+  } 
 }
-resource "yandex_vpc_network" "foo" {
-  name = "lab-network"
+#Network
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
 }
 
-
-resource "yandex_vpc_subnet" "foo" {
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
   zone           = "ru-central1-d"
-  network_id     = yandex_vpc_network.foo.id
+  network_id     = yandex_vpc_network.network-1.id
   v4_cidr_blocks = ["10.5.0.0/24"]
 }
 
-
-resource "yandex_vpc_address" "public" {
-  name = "example-public-address"
-
-  external_ipv4_address {
-    zone_id = "ru-central1-d"
-  }
+output "internal_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
 }
 
+output "external_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+}
+
+output "subnet-1" {
+  value = yandex_vpc_subnet.subnet-1.id
+}
+#Firewall
 resource "yandex_vpc_security_group" "group1" {
   name        = "My security group"
   description = "description for my security group"
-  network_id  = "${yandex_vpc_network.foo.id}"
+  network_id  = "${yandex_vpc_network.network-1.id}"
 
   labels = {
     my-label = "my-label-value"
@@ -79,17 +105,7 @@ resource "yandex_vpc_security_group_rule" "allow-http-ingress" {
   security_group_binding = yandex_vpc_security_group.group1.id
   direction              = "ingress"
   description            = "rule1 description"
-  v4_cidr_blocks         = ["10.0.1.0/24", "10.0.2.0/24"]
+  v4_cidr_blocks         = ["0.0.0.0/24"]
   port                   = 8080
   protocol               = "TCP"
-}
-
-resource "yandex_vpc_security_group_rule" "allow-http-egress" {
-  security_group_binding = yandex_vpc_security_group.group1.id
-  direction              = "egress"
-  description            = "rule2 description"
-  v4_cidr_blocks         = ["10.0.1.0/24"]
-  from_port              = 8090
-  to_port                = 8099
-  protocol               = "UDP"
 }
