815c5365c77636a5246d4ed214bf9283247e8cda
YoelRomero <dost22@yandex.ru>
2024-05-02 10:41:46 +0000

    Need to check this config, because http://ip:port doesn't work, other is ok

--- main.tf
+++ main.tf
@@ -31,18 +31,65 @@ resource "yandex_compute_instance" "example" {
   }
 
   network_interface {
-    subnet_id = yandex_vpc_subnet.foo.id
+    subnet_id = "${yandex_vpc_subnet.foo.id}"
   }
 
   metadata = {
     foo = "bar"
   }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'Hello, World!' > index.html",
+      "nohup busybox httpd -f -p 8080 &"
+    ]
+  }
 }
-resource "yandex_vpc_network" "foo" {}
+resource "yandex_vpc_network" "foo" {
+  name = "lab-network"
+}
 
 
 resource "yandex_vpc_subnet" "foo" {
   zone           = "ru-central1-d"
   network_id     = yandex_vpc_network.foo.id
   v4_cidr_blocks = ["10.5.0.0/24"]
+}
+
+
+resource "yandex_vpc_address" "public" {
+  name = "example-public-address"
+
+  external_ipv4_address {
+    zone_id = "ru-central1-d"
+  }
+}
+
+resource "yandex_vpc_security_group" "group1" {
+  name        = "My security group"
+  description = "description for my security group"
+  network_id  = "${yandex_vpc_network.foo.id}"
+
+  labels = {
+    my-label = "my-label-value"
+  }
+}
+
+resource "yandex_vpc_security_group_rule" "allow-http-ingress" {
+  security_group_binding = yandex_vpc_security_group.group1.id
+  direction              = "ingress"
+  description            = "rule1 description"
+  v4_cidr_blocks         = ["10.0.1.0/24", "10.0.2.0/24"]
+  port                   = 8080
+  protocol               = "TCP"
+}
+
+resource "yandex_vpc_security_group_rule" "allow-http-egress" {
+  security_group_binding = yandex_vpc_security_group.group1.id
+  direction              = "egress"
+  description            = "rule2 description"
+  v4_cidr_blocks         = ["10.0.1.0/24"]
+  from_port              = 8090
+  to_port                = 8099
+  protocol               = "UDP"
 }
