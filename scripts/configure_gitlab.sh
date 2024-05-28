fa0d017f647d3a088eaf17d47acba3d7bda5842a
Aizak <dost22@yandex.ru>
2024-05-29 00:29:26 +0300

    Add scripts

---
+++ scripts/configure_gitlab.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Правим конфигурационный файл GitLab
+sudo sed -i "s|external_url 'http://localhost'|external_url 'http://ip_address'|g" /etc/gitlab/gitlab.rb
+sudo sed -i "s|# gitlab_rails['initial_root_password'] = 'password'|gitlab_rails['initial_root_password'] = 'toortoor'|g" /etc/gitlab/gitlab.rb
+sudo sed -i "s|# gitlab_rails['initial_shared_runners_registration_token'] = 'token'|gitlab_rails['initial_shared_runners_registration_token'] = 'toortoor'|g" /etc/gitlab/gitlab.rb
+sudo sed -i "s|# gitlab_rails['store_initial_root_password'] = nil|gitlab_rails['store_initial_root_password'] = true|g" /etc/gitlab/gitlab.rb
+sudo sed -i "s|# nginx['enable'] = false|nginx['enable'] = true|g" /etc/gitlab/gitlab.rb
+sudo sed -i "s|# nginx['client_max_body_size'] = '250m'|nginx['client_max_body_size'] = '250m'|g" /etc/gitlab/gitlab.rb
+sudo sed -i "s|# nginx['redirect_http_to_https'] = false|nginx['redirect_http_to_https'] = false|g" /etc/gitlab/gitlab.rb
+sudo sed -i "s|# nginx['redirect_http_to_https_port'] = 80|nginx['redirect_http_to_https_port'] = 80|g" /etc/gitlab/gitlab.rb
+
+# Перезапускаем GitLab для применения изменений
+sudo gitlab-ctl reconfigure > gitlab.log 2>&1
+
+# Проверяем статус GitLab
+systemctl status gitlab-runsvdir.service
+sudo gitlab-ctl status
